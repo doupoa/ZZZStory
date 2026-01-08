e0d6c6e91c7b98ffd4c8623825a510cf9e72c3a0
VIRA <doupoa@qq.com>
2026-01-08 13:56:57 +0800

    转换剧情组件原生样式为tailwind类，避免全局样式污染

--- src/docs/components/style/Dialogue.vue
+++ src/docs/components/style/Dialogue.vue
@@ -54,8 +54,8 @@ export default {
 }
 </script>
 <template>
-    <div class="dialogue">
-        <span class="character" :style="{ color: roleColors[0], background: roleColors[1] }">{{ role }}</span>
+    <div class="my-6 relative pl-6 font-bold before:content-['“'] before:text-(--main-color-1) before:text-[2.50rem] before:leading-none before:absolute before:-top-2 before:left-0">
+        <span class="inline-block py-1 px-2 rounded text-[0.80rem] font-bold mr-2" :style="{ color: roleColors[0], background: roleColors[1] }">{{ role }}</span>
         <slot></slot>
     </div>
 </template>
--- src/docs/components/style/Scene.vue
+++ src/docs/components/style/Scene.vue
@@ -1,6 +1,6 @@
 <!-- 场景组件 -->
 <template>
-    <div class="scene">
+    <div class="block italic text-gray-400 my-6 pl-4 border-l-2 border-(--main-color-1)">
         <slot></slot>
     </div>
 </template>
