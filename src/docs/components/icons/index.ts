bbc25f99011c55e499df635c62834d41c366963d
VIRA <doupoa@qq.com>
2026-01-11 00:03:18 +0800

    fix: 完成绳网帖子信息改造,补充缺失图片及修正文本错误

--- src/docs/components/icons/index.ts
+++ src/docs/components/icons/index.ts
@@ -1,7 +1,7 @@
 import ViewsIcon from "./ViewsIcon.vue";
-import CreatorIcon from "./CreatorIcon.vue";
+import AuthorIcon from "./AuthorIcon.vue";
 import LikesIcon from "./LikesIcon.vue";
 
 // 图标使用 由TailwindCSS团队创作的 Heroicons (https://heroicons.com/)
 
-export { ViewsIcon, CreatorIcon, LikesIcon };
+export { ViewsIcon, AuthorIcon, LikesIcon };
--- src/docs/components/style/Dialogue.vue
+++ src/docs/components/style/Dialogue.vue
@@ -1,61 +1,68 @@
 <!-- 对话组件 -->
 
 <script lang="ts">
-import { computed } from 'vue';
+import { computed } from "vue";
 
-const roles = {// 第一个值为主色,第二个值为背景色
-    "random_play": { //录像店
-        "铃": ["#254fff", "#fd802dbf"],
-        "哲": ["#ffffff", "#fd802dbf"],
-        "Fairy": ["#1e3c72", "#1e3c7233"],
-        "(蓝发少女)": ["#254fff", "#fd802dbf"],
-        "(灰发少年)": ["#ffffff", "#fd802dbf"]
-    },
+const roles = {
+  // 第一个值为主色,第二个值为背景色
+  random_play: {
+    //录像店
+    铃: ["#254fff", "#fd802dbf"],
+    哲: ["#ffffff", "#fd802dbf"],
+    Fairy: ["#1e3c72", "#1e3c7233"],
+    "(蓝发少女)": ["#254fff", "#fd802dbf"],
+    "(灰发少年)": ["#ffffff", "#fd802dbf"],
+  },
 
-    "cunning_hares": { //狡兔屋
-        "安比": ["#b3cc58", "#C8E16C33"],
-        "比利": ["#cc4f4b", "#AF3E3A33"],
-        "妮可": ["#cd8583", "#E6ADAA33"],
-        "猫又": ["#A0351C", "#A0351C33"],
-        "(粉毛长发少女)": ["#cd8583", "#E6ADAA33"],
-        "(白毛短发少女)": ["#cc4f4b", "#AF3E3A33"]
-    },
-    "belobog_heavy_industries": {}, // 白祇重工
-    "sons_of_calydon": {},// 卡吕冬之子
-    "mockingbird": {}, //反舌鸟
+  cunning_hares: {
+    //狡兔屋
+    安比: ["#b3cc58", "#C8E16C33"],
+    比利: ["#cc4f4b", "#AF3E3A33"],
+    妮可: ["#cd8583", "#E6ADAA33"],
+    猫又: ["#A0351C", "#A0351C33"],
+    "(粉毛长发少女)": ["#cd8583", "#E6ADAA33"],
+    "(白毛短发少女)": ["#cc4f4b", "#AF3E3A33"],
+  },
+  belobog_heavy_industries: {}, // 白祇重工
+  sons_of_calydon: {}, // 卡吕冬之子
+  mockingbird: {}, //反舌鸟
 
-    "other": { "npc": ["dodgerblue", "#7c7c7c46"] } //其他角色
-
-}
+  other: { npc: ["dodgerblue", "#7c7c7c46"] }, //其他角色
+};
 export default {
-    props: {
-        role: {
-            type: String,
-            required: true,
-        }
+  props: {
+    role: {
+      type: String,
+      required: true,
     },
-    setup(props) {
-        const roleColors = computed(() => {
-            let colors = ['dodgerblue', "#7c7c7c46"]; // 默认颜色
-            // role 直接为角色名称，阵营名称仅做分类用
-            console.log(Object.keys(roles))
-            for (const camp of Object.keys(roles)) {
-                const campRoles = roles[camp];
-                if (campRoles[props.role]) {
-                    colors = campRoles[props.role];
-                    break;
-                }
-            }
-            return colors;
-        });
+  },
+  setup(props) {
+    const roleColors = computed(() => {
+      let colors = ["dodgerblue", "#7c7c7c46"]; // 默认颜色
+      // role 直接为角色名称，阵营名称仅做分类用
+      for (const camp of Object.keys(roles)) {
+        const campRoles = roles[camp];
+        if (campRoles[props.role]) {
+          colors = campRoles[props.role];
+          break;
+        }
+      }
+      return colors;
+    });
 
-        return { roleColors };
-    }
-}
+    return { roleColors };
+  },
+};
 </script>
 <template>
-    <div class="my-6 relative pl-6 font-bold before:content-['“'] before:text-(--main-color-1) before:text-[2.50rem] before:leading-none before:absolute before:-top-2 before:left-0">
-        <span class="inline-block py-1 px-2 rounded text-[0.80rem] font-bold mr-2" :style="{ color: roleColors[0], background: roleColors[1] }">{{ role }}</span>
-        <slot></slot>
-    </div>
+  <div
+    class="my-6 relative pl-6 font-bold before:content-['“'] before:text-(--main-color-1) before:text-[2.50rem] before:leading-none before:absolute before:-top-2 before:left-0"
+  >
+    <span
+      class="inline-block py-1 px-2 rounded text-[0.80rem] font-bold mr-2"
+      :style="{ color: roleColors[0], background: roleColors[1] }"
+      >{{ role }}</span
+    >
+    <slot></slot>
+  </div>
 </template>
